06630b6e250ec3e667ed3d31fe63c779852bc5b2
Polina <pparanich@ukr.net>
2025-04-25 00:03:50 +0300

    feat: product slider done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -9,13 +9,35 @@ function showMobileContainer() {
 burger.addEventListener('click', showMobileContainer);
 
 const swiper = new Swiper('.swiper-product', {
-    slidesPerView: 4,
-    loop: true,
-    speed: 500,
-    spaceBetween: 48,
-  
-    navigation: {
+  slidesPerView: 4,
+  loop: true,
+  speed: 500,
+  spaceBetween: 48,
+
+  navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
-  });
+  },
+
+  breakpoints: {
+      // when window width is >= 0px
+      0: {
+          slidesPerView: 1,
+          spaceBetween: 48,
+      },
+      // when window width is >= 480px
+      768: {
+          slidesPerView: 2,
+          spaceBetween: 48,
+      },
+      1125: {
+          slidesPerView: 3,
+          spaceBetween: 48,
+      },
+      // when window width is >= 640px
+      1500: {
+          slidesPerView: 4,
+          spaceBetween: 48,
+      }
+  }
+});
